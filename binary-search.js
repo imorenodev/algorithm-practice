1a12169771791b6adeebe0c9709cf0df89008bcc
imorenodev <imorenodev@gmail.com>
2016-02-08 15:54:13 -0500

    added function declarations, and appropriate console.log message depending on whether the target was found and how many tries it took

--- binary-search.js
+++ binary-search.js
@@ -1,26 +1,31 @@
-function binarySearch(arr, target) {
+var binarySearch = function(arr, target) {
   var max = arr.length-1,
       min = 0,
-      guess = Math.floor((max + min) / 2),
       count = 1;
   
-  function findTarget(nextGuess) {
+  var findTarget = function(nextGuess) {
     count++;
   	//base case
 	  if (arr[nextGuess] === target) {
-      console.log("Target found at index " + nextGuess + " after " + count + " guesses.");
       return nextGuess;
     }
 	  if (max < min) {
-      console.log("target not present in array");
       return -1;
     }
 	  if (arr[nextGuess] > target) max = nextGuess - 1;
 	  if (arr[nextGuess] < target) min = nextGuess + 1; 
 
-	  findTarget(Math.floor((max + min) / 2));
-	}
-	return findTarget(guess);
-}
+	  return findTarget(Math.floor((max + min) / 2));
+	};
+	return {
+    findTarget: findTarget(),
+    getCount: count
+  };
+};
 
-binarySearch([1,2,3,4,5,6,7,8,9,10], 6);
+var findNum = binarySearch([1,2,3,4,5,6,7,8,9,10], 10);
+console.log("The target was " + 
+	         (findNum.findTarget > -1 ? 
+	           "found at index " + findNum.findTarget : 
+	           "not found") + 
+	           " after " + findNum.getCount + " guesses.");
