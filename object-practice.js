462c62812bf36b6b8e7a6e16e7075472c894e41f
imorenodev <imorenodev@gmail.com>
2016-02-16 00:20:34 -0500

    Practice with objects

---
+++ object-practice.js
@@ -0,0 +1,28 @@
+var Person = function(name) {
+  var firstName = name.substr(0, name.indexOf(" "));
+  var lastName = name.substr(name.indexOf(" ")+1, name.length-1);
+
+  this.setFullName = function(name) {
+    firstName = this.setFirstName(name.substr(0, name.indexOf(" ")));
+    lastName = this.setLastName(name.substr(name.indexOf(" ")+1, name.length-1));
+  };
+  this.setFirstName = function(name) {
+    firstName = name;
+  };
+  this.setLastName = function(name) {
+    lastName = name;
+  };
+  this.getFullName = function() {
+    return firstName, lastnName;
+  };
+  this.getFirstName = function() {
+    return firstName;
+  };
+  this.getLastName = function() {
+    return lastName;
+  };
+};
+
+var ian = new Person("Ian Moreno");
+var show = ian.getFullName();
+console.log(show);
